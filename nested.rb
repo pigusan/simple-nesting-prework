d2a03ab7e652559a86d02630e182dc85eabe8c62
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-27 20:59:38 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -74,18 +74,18 @@ def adding_matz
 				:known_for => "Ruby",
 			  :languages => ["LISP, C"]
 			},
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
      }
 
 
