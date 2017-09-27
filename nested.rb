a6e667b55f062e74a59a1ca3bb1948c6d657b557
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-27 21:25:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,6 @@ def changing_alan
      alans_new_info = "GUI"
 		programmer_hash[:alan_kay][:known_for] =  alans_new_info
 		programmer_hash
-
 end
 
 def adding_to_dennis
@@ -126,5 +125,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+		 programmer_hash[:dennis_ritchie][:languages][1]= "Assembly"
 end
